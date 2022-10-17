fbd61e3b3959f3d22479c3f00c6350a94e8ae0f8
Lô Vỹ Vy Vy Vy Vy Vy <93115152+AnPhua@users.noreply.github.com>
2022-10-17 17:07:09 +0700

    Add files via upload

---
+++ demoDatabaseconnect/demoDatabaseconnect/Form1.Designer.cs
@@ -0,0 +1,228 @@
+﻿namespace demoDatabaseconnect
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle1 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle2 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.label1 = new System.Windows.Forms.Label();
+            this.txtMaCL = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtTenCL = new System.Windows.Forms.TextBox();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.btnThemMoi = new System.Windows.Forms.Button();
+            this.btnLuu = new System.Windows.Forms.Button();
+            this.btnSua = new System.Windows.Forms.Button();
+            this.btnXoa = new System.Windows.Forms.Button();
+            this.btnBoQua = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label1.Location = new System.Drawing.Point(49, 35);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 20);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "Mã Chất Liệu";
+            // 
+            // txtMaCL
+            // 
+            this.txtMaCL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtMaCL.Location = new System.Drawing.Point(180, 35);
+            this.txtMaCL.Name = "txtMaCL";
+            this.txtMaCL.Size = new System.Drawing.Size(312, 26);
+            this.txtMaCL.TabIndex = 1;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.DeepSkyBlue;
+            this.label2.Location = new System.Drawing.Point(49, 122);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(121, 20);
+            this.label2.TabIndex = 0;
+            this.label2.Text = "Tên Chất Liệu";
+            // 
+            // txtTenCL
+            // 
+            this.txtTenCL.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtTenCL.Location = new System.Drawing.Point(180, 119);
+            this.txtTenCL.Name = "txtTenCL";
+            this.txtTenCL.Size = new System.Drawing.Size(312, 26);
+            this.txtTenCL.TabIndex = 1;
+            // 
+            // dataGridView1
+            // 
+            this.dataGridView1.BackgroundColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleCenter;
+            dataGridViewCellStyle1.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle1.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle1.ForeColor = System.Drawing.SystemColors.WindowText;
+            dataGridViewCellStyle1.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle1.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle1.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle1;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
+            dataGridViewCellStyle2.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle2.BackColor = System.Drawing.SystemColors.Window;
+            dataGridViewCellStyle2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            dataGridViewCellStyle2.ForeColor = System.Drawing.SystemColors.ControlText;
+            dataGridViewCellStyle2.SelectionBackColor = System.Drawing.SystemColors.Highlight;
+            dataGridViewCellStyle2.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle2.WrapMode = System.Windows.Forms.DataGridViewTriState.False;
+            this.dataGridView1.DefaultCellStyle = dataGridViewCellStyle2;
+            this.dataGridView1.GridColor = System.Drawing.Color.Lavender;
+            this.dataGridView1.Location = new System.Drawing.Point(52, 185);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.Size = new System.Drawing.Size(400, 281);
+            this.dataGridView1.TabIndex = 2;
+            this.dataGridView1.CellClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellClick);
+            this.dataGridView1.CellContentClick += new System.Windows.Forms.DataGridViewCellEventHandler(this.dataGridView1_CellContentClick);
+            this.dataGridView1.Click += new System.EventHandler(this.dataGridView1_Click);
+            // 
+            // btnThemMoi
+            // 
+            this.btnThemMoi.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnThemMoi.ForeColor = System.Drawing.Color.Crimson;
+            this.btnThemMoi.Location = new System.Drawing.Point(604, 42);
+            this.btnThemMoi.Name = "btnThemMoi";
+            this.btnThemMoi.Size = new System.Drawing.Size(145, 45);
+            this.btnThemMoi.TabIndex = 3;
+            this.btnThemMoi.Text = "Thêm";
+            this.btnThemMoi.UseVisualStyleBackColor = true;
+            this.btnThemMoi.Click += new System.EventHandler(this.btnThemMoi_Click);
+            // 
+            // btnLuu
+            // 
+            this.btnLuu.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnLuu.ForeColor = System.Drawing.Color.Crimson;
+            this.btnLuu.Location = new System.Drawing.Point(604, 109);
+            this.btnLuu.Name = "btnLuu";
+            this.btnLuu.Size = new System.Drawing.Size(145, 45);
+            this.btnLuu.TabIndex = 3;
+            this.btnLuu.Text = "Lưu";
+            this.btnLuu.UseVisualStyleBackColor = true;
+            this.btnLuu.Click += new System.EventHandler(this.btnLuu_Click);
+            // 
+            // btnSua
+            // 
+            this.btnSua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnSua.ForeColor = System.Drawing.Color.Crimson;
+            this.btnSua.Location = new System.Drawing.Point(604, 185);
+            this.btnSua.Name = "btnSua";
+            this.btnSua.Size = new System.Drawing.Size(145, 45);
+            this.btnSua.TabIndex = 3;
+            this.btnSua.Text = "Sửa";
+            this.btnSua.UseVisualStyleBackColor = true;
+            this.btnSua.Click += new System.EventHandler(this.btnSua_Click);
+            // 
+            // btnXoa
+            // 
+            this.btnXoa.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnXoa.ForeColor = System.Drawing.Color.Crimson;
+            this.btnXoa.Location = new System.Drawing.Point(604, 252);
+            this.btnXoa.Name = "btnXoa";
+            this.btnXoa.Size = new System.Drawing.Size(145, 45);
+            this.btnXoa.TabIndex = 3;
+            this.btnXoa.Text = "Xóa";
+            this.btnXoa.UseVisualStyleBackColor = true;
+            this.btnXoa.Click += new System.EventHandler(this.btnXoa_Click);
+            // 
+            // btnBoQua
+            // 
+            this.btnBoQua.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btnBoQua.ForeColor = System.Drawing.Color.Crimson;
+            this.btnBoQua.Location = new System.Drawing.Point(604, 325);
+            this.btnBoQua.Name = "btnBoQua";
+            this.btnBoQua.Size = new System.Drawing.Size(145, 45);
+            this.btnBoQua.TabIndex = 3;
+            this.btnBoQua.Text = "Bỏ Qua";
+            this.btnBoQua.UseVisualStyleBackColor = true;
+            this.btnBoQua.Click += new System.EventHandler(this.btnBoQua_Click);
+            // 
+            // button6
+            // 
+            this.button6.FlatAppearance.BorderSize = 0;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button6.ForeColor = System.Drawing.Color.Crimson;
+            this.button6.Location = new System.Drawing.Point(604, 392);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(145, 45);
+            this.button6.TabIndex = 3;
+            this.button6.Text = "Thoát";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(823, 566);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.btnBoQua);
+            this.Controls.Add(this.btnXoa);
+            this.Controls.Add(this.btnSua);
+            this.Controls.Add(this.btnLuu);
+            this.Controls.Add(this.btnThemMoi);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.txtTenCL);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.txtMaCL);
+            this.Controls.Add(this.label1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox txtMaCL;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtTenCL;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button btnThemMoi;
+        private System.Windows.Forms.Button btnLuu;
+        private System.Windows.Forms.Button btnSua;
+        private System.Windows.Forms.Button btnXoa;
+        private System.Windows.Forms.Button btnBoQua;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ demoDatabaseconnect/demoDatabaseconnect/Form1.cs
@@ -0,0 +1,200 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+namespace demoDatabaseconnect
+{
+    public partial class Form1 : Form
+    {
+        SqlConnection con ;
+        public Form1()
+        {
+            InitializeComponent();
+            con = new SqlConnection(@"Data Source=LAPTOP-AUDFFK8M;Initial Catalog=QLyCL;Integrated Security=True");
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+  
+        
+            DataTable dtChatLieu = ProcessDataBase.DocBang("select * from tblChatLieu");
+            dataGridView1.DataSource = dtChatLieu;
+            //Định dạng dataGrid
+            dataGridView1.Columns[0].HeaderText = "Mã chất liệu";//dgvChatLieu
+            dataGridView1.Columns[1].HeaderText = "Tên chất liệu";
+            dataGridView1.Columns[0].Width = 150;
+            dataGridView1.Columns[1].Width = 250;
+            dataGridView1.BackgroundColor = Color.Black;
+            dtChatLieu.Dispose();//Giải phóng bộ nhớ cho DataTable
+
+            btnXoa.Enabled = false;
+            btnSua.Enabled = false;
+            btnLuu.Enabled = false;
+            btnBoQua.Enabled = false;
+            btnThemMoi.Enabled = true;
+
+        }
+
+        private void dataGridView1_CellContentClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_CellClick(object sender, DataGridViewCellEventArgs e)
+        {
+
+        }
+
+        private void dataGridView1_Click(object sender, EventArgs e)
+        {
+            txtMaCL.Text = dataGridView1.CurrentRow.Cells[0].Value.ToString();
+            txtTenCL.Text = dataGridView1.CurrentRow.Cells[1].Value.ToString();
+
+            txtMaCL.Enabled = false;
+            btnXoa.Enabled = true;
+            btnSua.Enabled = true;
+            btnLuu.Enabled = false;
+            btnBoQua.Enabled = true;
+            btnThemMoi.Enabled = false;
+
+        }
+
+        private void btnSua_Click(object sender, EventArgs e)
+        {
+            if (txtTenCL.Text == "")
+            {
+                MessageBox.Show("Bạn phải nhập tên chất liệu", "Thông báo",
+                     MessageBoxButtons.OK, MessageBoxIcon.Information);
+                txtTenCL.Focus();
+
+            }
+            else
+            {
+                ProcessDataBase.CapNhatDuLieu("update tblChatLieu set TenChatLieu=N'" + txtTenCL.Text + "' where MaChatLieu='" + txtMaCL.Text + "'");
+                ResetValue();//Xóa dữ liệu ở các ô nhập TextBox
+                             //Sau khi update cần lấy lại dữ liệu để hiển thị lên lưới
+                dataGridView1.DataSource = ProcessDataBase.DocBang("select * from tblChatLieu");
+
+                btnXoa.Enabled = false;
+                btnSua.Enabled = false;
+                btnLuu.Enabled = false;
+                btnBoQua.Enabled = false;
+                btnThemMoi.Enabled = true;
+            }
+
+        }
+        public void ResetValue()
+        {
+            txtMaCL.Text = "";
+            txtTenCL.Text = "";
+        }
+
+        private void btnXoa_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn xóa chất liệu có mã là:" +
+               txtMaCL.Text + " không?", "Thông báo",
+               MessageBoxButtons.YesNo, MessageBoxIcon.Question) ==
+               System.Windows.Forms.DialogResult.Yes)
+            {
+                ProcessDataBase.CapNhatDuLieu("delete tblChatLieu where MaChatLieu='" + txtMaCL.Text + "'");
+                dataGridView1.DataSource = ProcessDataBase.DocBang("Select * from tblChatLieu");
+                ResetValue();
+                btnXoa.Enabled = false;
+                btnSua.Enabled = false;
+                btnLuu.Enabled = false;
+                btnBoQua.Enabled = false;
+                btnThemMoi.Enabled = true;
+            }
+
+        }
+
+        private void btnThemMoi_Click(object sender, EventArgs e)
+        {
+            ResetValue();
+            txtMaCL.Enabled = true;
+            txtMaCL.Focus();
+
+            btnXoa.Enabled = false;
+            btnSua.Enabled = false;
+            btnLuu.Enabled = true;
+            btnBoQua.Enabled = true;
+            btnThemMoi.Enabled = true;
+
+        }
+
+        private void btnLuu_Click(object sender, EventArgs e)
+        {
+            if (txtMaCL.Text == "")
+            {
+                MessageBox.Show("Bạn phải nhập mã chất liệu");
+                txtMaCL.Focus();
+            }
+            else
+            {
+                if (txtTenCL.Text == "")
+                {
+                    MessageBox.Show("Bạn phải nhập tên chất liệu");
+                    txtTenCL.Focus();
+                }
+                else
+                {
+                    DataTable dtChatLieu = ProcessDataBase.DocBang("Select * from tblChatLieu where" + " MaChatLieu='" + (txtMaCL.Text).Trim() + "'");
+                    if (dtChatLieu.Rows.Count > 0)
+                    {
+                        MessageBox.Show("Mã chất liệu này đã có, hãy nhập mã khác!");
+                        txtMaCL.Focus();
+                    }
+                    else
+                    {
+                        ProcessDataBase.CapNhatDuLieu("insert into tblChatLieu values(N'" + txtMaCL.Text + "',N'" + txtTenCL.Text + "')");
+                        MessageBox.Show("Bạn đã thêm mới thành công");
+                        dataGridView1.DataSource = ProcessDataBase.DocBang("select * from tblChatLieu");
+                        ResetValue();
+                        btnXoa.Enabled = false;
+                        btnSua.Enabled = false;
+                        btnLuu.Enabled = false;
+                        btnBoQua.Enabled = false;
+                        btnThemMoi.Enabled = true;
+                    }
+                }
+            }
+
+        }
+
+        private void btnBoQua_Click(object sender, EventArgs e)
+        {
+            ResetValue();
+            btnXoa.Enabled = false;
+            btnSua.Enabled = false;
+            btnLuu.Enabled = false;
+            btnBoQua.Enabled = false;
+            btnThemMoi.Enabled = true;
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            if (MessageBox.Show("Bạn có muốn thoát không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question)
+                == System.Windows.Forms.DialogResult.Yes)
+            {
+                if (btnLuu.Enabled == true)
+                {
+                    if (MessageBox.Show("Bạn có muốn Lưu lại chất liệu đang thêm mới không?", "Thông báo", MessageBoxButtons.YesNo, MessageBoxIcon.Question) == System.Windows.Forms.DialogResult.Yes)
+                        btnLuu_Click(sender, e);
+                    else
+                        this.Close();
+
+                }
+                else
+                    this.Close();
+            }
+
+        }
+    }
+}
